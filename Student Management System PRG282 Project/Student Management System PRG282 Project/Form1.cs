55079f47804a484bf66f220bdf1519e709134b7f
Llewellyn Jakobus Fourie <123295717+Llewellyn04@users.noreply.github.com>
2024-10-29 16:50:03 +0200

    Add files via upload
    Repository created

---
+++ Student Management System PRG282 Project/Student Management System PRG282 Project/Form1.cs
@@ -0,0 +1,10 @@
+namespace Student_Management_System_PRG282_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
